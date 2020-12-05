359048848f0492f3cdc2d4fa84915db3af01ffa6
user <fatemehrezai1994@gmail.com>
2020-12-05 15:28:46 +0330

    change productCategoryChecker for geting from local storage

--- src/_helpers/productCategoryChecker.js
+++ src/_helpers/productCategoryChecker.js
@@ -1,23 +1,31 @@
 import clothing_productArray from '../_screens/clothing/_const/ClothingـProductSampleList';
 import fruit_productArray from '../_screens/fruit/_const/FruitـProductSampleList';
 import homeAppliances_productArray from '../_screens/homeAppliances/_const/HomeAppliancesـProductSampleList';
+import { get } from "./store";
+
 
 
 export function productCategoryChecker(productCategory) {
+    //get data from Local storage
+    const _clothing_productArray = get("clothing_productArray");
+    const _fruit_productArray = get("fruit_productArray");
+    const _homeAppliances_productArray = get("home-appliances_productArray");
+
     let productArray=[];
     switch (productCategory) {
         case "fruit":
-            productArray = fruit_productArray;
+            productArray = _fruit_productArray || fruit_productArray;
             break;
         case "clothing":
-            productArray = clothing_productArray;
+            productArray = _clothing_productArray || clothing_productArray;
             break;
         case "home-appliances":
-            productArray = homeAppliances_productArray;
+            productArray = _homeAppliances_productArray || homeAppliances_productArray;
             break;    
         default:
-            productArray = fruit_productArray;
+            productArray = _fruit_productArray || fruit_productArray;
             break;
     }
     return productArray;
 }
+
